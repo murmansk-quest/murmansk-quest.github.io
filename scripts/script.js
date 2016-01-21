5b06b1998e1357be1977eb145d82050d1ea52357
Sergey Pimenov <quest.in.murmansk@gmail.com>
2016-01-21 20:55:13 +0300

    Schedule day 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,14 @@
+// Set height of quest_article. Height = width / 1.7
+
 var articles = document.querySelectorAll(".quests_block article");
+
+var price = document.querySelectorAll(".price");
+
+var time = document.querySelectorAll(".price .time");
+
+if(price[1].childNodes.length == 9) {
+	alert("!")
+}
 
 function articleSize(){
 	var article_width = getComputedStyle(articles[0]).width;
@@ -11,3 +21,18 @@ function articleSize(){
 articleSize();
 
 window.addEventListener("resize", articleSize);
+
+
+// For Microsoft browsers
+
+if (document.documentMode || /Edge/.test(navigator.userAgent)) {
+   var articles_q = document.querySelectorAll(".quests_block article");
+   for( i = 0; articles_q.length; i++) {
+   		articles_q[i].style.minHeight = "350px";
+   }
+}
+
+
+// Shedule
+
+	// Shedule main variables
