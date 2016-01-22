f562e5633c6fdf26b84a9985c57a1b8c60d64d2d
Sergey Pimenov <quest.in.murmansk@gmail.com>
2016-01-22 22:41:37 +0300

    Schedule

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,64 @@
-// Set height of quest_article. Height = width / 1.7
+/* Circles position */
 
-var articles = document.querySelectorAll(".quests_block article");
+// Price and time variables
 
 var price = document.querySelectorAll(".price");
 
+var price_first = document.querySelectorAll(".price:first-of-type");
+
+var price_last = document.querySelectorAll(".price:last-of-type");
+
 var time = document.querySelectorAll(".price .time");
 
-if(price[1].childNodes.length == 9) {
-	alert("!")
+var second_price = document.querySelectorAll(".second_price");
+
+// Find circles quantity in price_block, then set background-position
+
+for(i = 0; i < price_first.length; i++ ) {
+
+	if(price_first[i].childNodes.length == 13) {
+		price_first[i].style.backgroundPosition = "0% 90%, 102% 90%, 102.5% 83%";
+	} else if (price_first[i].childNodes.length == 9) {
+		price_first[i].style.backgroundPosition = "0% 90%, 103.5% 90%, 102.5% 83%";
+	} else if (price_first[i].childNodes.length == 7) {
+		price_first[i].style.backgroundPosition = "0% 90%, 105% 90%, 102.5% 83%";
+	}
+
 }
+
+for( i = 0; i < second_price.length; i++ ) {
+
+	if(second_price[i].childNodes.length == 7) {
+		second_price[i].style.backgroundPosition = "-3.9% 90%, 104% 90%, 102.5% 83%";
+	} else if(second_price[i].childNodes.length == 11) {
+		second_price[i].style.backgroundPosition = "-2.3% 90%, 101.9% 90%, 102.5% 83%";
+	}
+
+}
+
+for(i = 0; i < price_last.length; i++ ) {
+
+	if(price_last[i].childNodes.length == 9) {
+		price_last[i].style.backgroundPosition = "-3% 90%, 102.5% 83%";
+	} else if(price_last[i].childNodes.length == 11) {
+		price_last[i].style.backgroundPosition = "-2% 90%, 102.5% 83%";
+	} else if(price_last[i].childNodes.length == 13) {
+		price_last[i].style.backgroundPosition = "-2% 90%, 102.5% 83%";
+	} else if(price_last[i].childNodes.length == 15) {
+		price_last[i].style.backgroundPosition = "-1.8% 90%, 102.5% 83%";
+	} else if(price_last[i].childNodes.length == 17) {
+		price_last[i].style.backgroundPosition = "-1.2% 90%, 102.5% 83%";
+	}
+
+}
+
+/* Circles position end */
+
+
+
+// Set height of quest_article. Height = width / 1.7
+
+var articles = document.querySelectorAll(".quests_block article");
 
 function articleSize(){
 	var article_width = getComputedStyle(articles[0]).width;
@@ -23,6 +73,7 @@ articleSize();
 window.addEventListener("resize", articleSize);
 
 
+
 // For Microsoft browsers
 
 if (document.documentMode || /Edge/.test(navigator.userAgent)) {
@@ -31,8 +82,3 @@ if (document.documentMode || /Edge/.test(navigator.userAgent)) {
    		articles_q[i].style.minHeight = "350px";
    }
 }
-
-
-// Shedule
-
-	// Shedule main variables
