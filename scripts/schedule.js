1310c9d8bcd7af39693cd0cb6243272efdce91cb
Sergey Pimenov <quest.in.murmansk@gmail.com>
2016-01-24 23:56:50 +0300

    Schedule

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -1,3 +1,28 @@
 var set_month = "october";
 
-var day_in_month = "31"
+var day_in_month = "31";
+
+var first_price = "2000rub";
+
+var second_price = "2500rub";
+
+var third_price = "3000rub";
+
+var price_one = document.querySelectorAll(".price_one");
+var price_two = document.querySelectorAll(".price_two");
+var price_tree = document.querySelectorAll(".price_tree");
+
+for( i = 0; i < price_one.length; i++ ) {
+	price_one[i].innerHTML = first_price;
+}
+
+for( i = 0; i < price_two.length; i++ ) {
+	price_two[i].innerHTML = second_price;
+}
+
+for( i = 0; i < price_tree.length; i++ ) {
+	price_tree[i].innerHTML = third_price;
+}
+
+
+
