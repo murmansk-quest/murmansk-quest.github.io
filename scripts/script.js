a97a5c566fa231fb8fe0033afedb5ac06c1ed85d
Sergey Pimenov <fastralserg@gmail.com>
2016-02-25 17:56:31 +0300

    Modal

--- scripts/script.js
+++ scripts/script.js
@@ -599,8 +599,9 @@ function getTime(event) {
 		left_aside_modal.classList.remove("quest_tree_modal");
 		left_aside_modal.classList.add("quest_two_modal");
 		modal_description.innerHTML = "Описание квеста \" Грабители \" ";
+		modal_description_container.classList.remove("modal_description_wizard");
 		modal_description_container.classList.add("modal_description_robbers");
-		
+
 	}
 	if( target.parentNode.parentNode.classList.contains("price_block_3") ) {
 		quest_name_modal.innerHTML = quest_tree;
@@ -608,6 +609,8 @@ function getTime(event) {
 		left_aside_modal.classList.remove("quest_two_modal");
 		left_aside_modal.classList.add("quest_tree_modal");
 		modal_description.innerHTML = "Старый маг не справился и ценой своей жизни запечатал помещение, дабы чудовищный дух не вырвался на свободу. ";
+		modal_description_container.classList.remove("modal_description_robbers");
+		modal_description_container.classList.add("modal_description_wizard");
 	}
 }
 
